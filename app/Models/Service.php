d99d54dab4b7cb76693b0ee11df37bf2b1be24c6
fkula5 <f3kula@gmail.com>
2024-04-24 13:23:22 +0200

    feat: employees services pivot

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Service extends Model
@@ -18,5 +19,10 @@ class Service extends Model
     public function appointments(): HasMany
     {
         return $this->hasMany(Appointment::class);
+    }
+
+    public function employees(): BelongsToMany
+    {
+        return $this->belongsToMany(Employee::class);
     }
 }
